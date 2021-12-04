e3132f2778abc42caaf186bd4ba62ae1d55100aa
Jeremyas Cornelis <74807111+jeremyascornelis@users.noreply.github.com>
2021-12-04 22:55:59 +0700

    Update amazonscrap-pricechecker-item.py
    Edited the comment section

--- python_simple_project/amazonscrap-pricechecker-item.py
+++ python_simple_project/amazonscrap-pricechecker-item.py
@@ -5,7 +5,7 @@ import smtplib
 # url = "https://www.amazon.co.uk/Apple-Airpods-Wireless-Charging-latest/dp/B07PYM8FB8/ref=sr_1_6?keywords=airpods&qid=1581244893&sr=8-6"
 url = "https://www.amazon.co.uk/OnePlus-Buds-Pro-headphones-Cancellation-White/dp/B07XW425NC/ref=sr_1_1?crid=1KWZIIOZ7KSNW&keywords=airpods%2Bpro&qid=1638612493&sprefix=airpods%2Caps%2C1174&sr=8-1&th=1" #the url specific of product item that you want to scrap in amazon web
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"}
-price_value = 147 #this is the amount that you want the item to drop to in price. This is the price that maximum price that you will able to purchase the item
+price_value = 147 #this is the amount that you want the item to drop to in price. This is maximum budget that you will able to purchase the item.
 email_address = "youremailaddress@gmail.com"#this is the account the python will interact with to send an email when the price drops to the specified
 password = "youremailpassword" #youremailpassword
 receiver_email = "youranotheremail@gmail.com" #the e-mail address that will receive the notification that the price has dropped to the range you're tracking
